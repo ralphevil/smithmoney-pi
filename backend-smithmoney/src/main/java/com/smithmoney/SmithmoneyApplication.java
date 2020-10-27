3fd22f9fc2ebdf9644f37098cf877366c7c59124
Thatiany Steer <steermkt@gmail.com>
2020-10-27 11:05:20 -0300

    teste

--- backend-smithmoney/src/main/java/com/smithmoney/SmithmoneyApplication.java
+++ backend-smithmoney/src/main/java/com/smithmoney/SmithmoneyApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmithmoneyApplication {
-
+	puclic static String ttr;
 	public static void main(String[] args) {
 		SpringApplication.run(SmithmoneyApplication.class, args);
 	}
 
 }
+
+
