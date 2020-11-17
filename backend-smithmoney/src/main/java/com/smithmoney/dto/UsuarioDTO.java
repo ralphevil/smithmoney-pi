5d686b9176b8de59959fdbdde98ba4fd30c557de
Gagego Maciel <gagego.maciel@gmail.com>
2020-11-17 20:05:14 -0300

    Resolvendo conflito

--- backend-smithmoney/src/main/java/com/smithmoney/dto/UsuarioDTO.java
+++ backend-smithmoney/src/main/java/com/smithmoney/dto/UsuarioDTO.java
@@ -3,12 +3,9 @@ package com.smithmoney.dto;
 import java.time.LocalDate;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,7 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UsuarioDTO {
-
+	
 	private Long id;
 	
     @Size(min =6, max = 60)
