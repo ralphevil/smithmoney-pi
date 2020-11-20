93fbf7d51dc654b6cba5abe042f80ce7ab9ff6d0
jotapelc <joao.paulo.c@hotmail.com>
2020-11-20 03:05:45 -0300

    ajuste de contas edição e exclusão

--- frontend/js/contascript.js
+++ frontend/js/contascript.js
@@ -1,3 +1,13 @@
+jQuery(function($){
+    $('.conta-saldo').mask('#.##0,00', {reverse: true});
+    });
+
+jQuery(function($){
+    $('.editar-conta-saldo').mask('#.##0,00', {reverse: true});
+    });
+
+
+/* 
 let nomeConta = document.querySelector("#exampleFormControlInput1");
 let saldoAtual = document.querySelector("#exampleFormControlInput2");
 let banco = document.querySelector("#exampleFormControlSelect1");
@@ -28,5 +38,34 @@ cadastroConta.addEventListener("click", function(event) {
         alert("Conta adicionada com sucesso!")
     })
     
-})
+}) */
 
+/* function cadastrarUsuario(event, form) {
+    event.preventDefault();
+    const nome = form.nome.value;
+    const email = form.email.value;
+    const senha = form.senha.value;
+    const data = {
+      nome: nome,
+      email: email,
+      password: senha
+    }
+
+    if ((!nome.length <= 0 || !nome == "") && (!email.length <= 0 || !email == "") && (!senha.length <= 0 || !senha == 0)) {
+      fetch('http://localhost:8080/api/usuarios', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+      })
+        .then(function (data) {
+          console.log('Request success: ', data);
+          if (data.ok) window.location.href = "/login.html"
+        })
+        .catch(function (error) {
+          console.log('Request failure: ', error);
+        });
+    }
+  } */
