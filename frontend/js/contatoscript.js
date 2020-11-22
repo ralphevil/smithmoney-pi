bf5c8d18fde650e54ef596ab302d739d7d49431f
Gusotavu <guga.trabalhos2000@gmail.com>
2020-11-22 17:31:20 -0300

    contato
    pequenas alteraçoes sobre contato

--- frontend/js/contatoscript.js
+++ frontend/js/contatoscript.js
@@ -1,5 +1,5 @@
 let nome = document.querySelector("#nome");
-let email = document.querySelector("#email");
+let emails = document.querySelector("#emails");
 let assunto = document.querySelector("#assunto");
 let menssagem = document.querySelector("#menssagem");
 let formulario = document.querySelector("#formulario");
@@ -13,17 +13,6 @@ formulario.addEventListener("submit", function(event){
         assunto: assunto.value,
         menssagem: menssagem.value
     };
-
-    fetch("http://apiurl", {
-        method: "POST",
-        body: JSON.stringify(conteudo)
-    })
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        alert("Sua menságem foi enviada com sucesso! Agora é só aguardar, breve entaremos em contato, te mais. ^^")
-    })
 
 
 })
