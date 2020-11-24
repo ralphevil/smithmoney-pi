57cb0cfe228e6a389ad49407cd70437b08a533e4
Gagego Maciel <gagego.maciel@gmail.com>
2020-11-24 09:59:41 -0300

    Ajustando Daschboard e Despesa e receitas

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,3 +1,4 @@
+
 let btnAdd = document.querySelector("#btn-adiciona");
 let btnFechar = document.querySelector('#btn-fechar');
 
@@ -20,3 +21,56 @@ function iniciaModal(modalId) {
     });
   }
 }
+
+
+(function() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:3333/despesas");
+
+  xhr.addEventListener("load", function() {
+    let resposta = xhr.responseText;
+    let despesas = JSON.parse(resposta);
+    despesas.map((despesa) => {
+      let buscaDespesa = obtemDadosDespesas(despesa);
+      let trDespesas = montaTr(buscaDespesa);
+      let tabelaDespesas = document.querySelector("#tabela-pendencia");
+      tabelaDespesas.appendChild(trDespesas);
+    });
+
+    function obtemDadosDespesas(despesa) {
+      let despesas = {
+        valor: despesa.valor,
+        data: despesa.dataVencimento,
+        descricao: despesa.descricao,
+        categorias: despesa.categoria,
+      }
+      return despesas;
+    }
+
+    function montaTr(despesa) {
+      let trDespesa = document.createElement("tr");
+      trDespesa.classList.add("despesas");
+    
+      trDespesa.appendChild(montaTd('Pendente', "info-status"))
+      trDespesa.appendChild(montaTd(despesa.data, "info-data"));
+      trDespesa.appendChild(montaTd(despesa.descricao, "info-descricao"));
+      trDespesa.appendChild(montaTd(despesa.categorias, "info-categoria"));
+      trDespesa.appendChild(montaTd(despesa.contas, "info-contas"));
+      trDespesa.appendChild(montaTd(despesa.valor, "info-valor"));
+      trDespesa.appendChild(montaTd("Efetivar", "info-efetiva"));
+      trDespesa.appendChild(montaTd("Editar", "info-altera"));
+      trDespesa.appendChild(montaTd("Deletar", "info-delete"));
+    
+      return trDespesa;
+    }
+
+    function  montaTd(dado, classe) {
+      let td = document.createElement("td");
+      td.textContent = dado;
+      td.classList.add(classe);
+      return td;
+    }
+  });
+
+  xhr.send();
+})();
--- frontend/js/graficos.js
+++ frontend/js/graficos.js
@@ -1,13 +1,21 @@
 var ctx1 = document.getElementsByClassName("categoria-despesas");
-let ctx2 = document.getElementsByClassName("categoria-receitas");
+var ctx2 = document.getElementsByClassName("categoria-receitas");
+var ctx3 = document.getElementsByClassName("balanco");
 
-var chartGraph = new Chart(ctx1, {
+let despesa = {
+  categoria: "Alimnetação",
+  valor: 15
+}
+
+let arr = ["Alimentação", "Lazer", "Saúde", "Trasporte", "Moradia", "vestuariooooo"];
+
+var grafico01 = new Chart(ctx1, {
   type: 'doughnut',
   data: {
-    labels: ["Alimentação", "Lazer", "Transportes", "Moradia", "Vestuario"],
+    labels: arr,
     datasets: [{
       label: "Despesas por categoria",
-      data: [100, 200, 50, 60, 70, 300],
+      data: ["350", 200, 50, 60, 70, 300],
       borderWidth: 3,
       backgroundColor: [
             'rgba(255, 99, 132, 0.9)',
@@ -15,8 +23,7 @@ var chartGraph = new Chart(ctx1, {
             'rgba(255, 206, 86, 0.9)',
             'rgba(75, 192, 192, 0.9)',
             'rgba(153, 102, 255, 0.9)',
-            'rgba(255, 159, 64, 0.9)',
-            'rgba(54, 162, 235, 0.9)'
+            'rgba(255, 159, 64, 0.9)'
         ],
         borderColor: [
             'rgba(255, 99, 132, 0.9)',
@@ -24,20 +31,19 @@ var chartGraph = new Chart(ctx1, {
             'rgba(255, 206, 86, 0.9)',
             'rgba(75, 192, 192, 0.9)',
             'rgba(153, 102, 255, 0.9)',
-            'rgba(255, 159, 64, 0.9)',
-            'rgba(54, 162, 235, 0.9)'
+            'rgba(255, 159, 64, 0.9)'
         ],
     }]
   }
 });
 
-var chartGraph = new Chart(ctx2, {
+var grafico02 = new Chart(ctx2, {
   type: 'doughnut',
   data: {
     labels: ["Salário", "Alugueis", "Dividendos"],
     datasets: [{
-      label: "Despesas por categoria",
-      data: [1500.00, 2.000, 500],
+      label: "Receitas por categoria",
+      data: [1500, 2000, 500],
       borderWidth: 3,
       backgroundColor: [
           'rgba(255, 99, 132, 0.9)',
@@ -50,6 +56,36 @@ var chartGraph = new Chart(ctx2, {
           'rgba(255, 206, 86, 0.9)' 
         ],
     }]
+  }
+});
+
+var grafico03 = new Chart(ctx3, {
+  type: 'bar',
+  data: {
+      labels: ['Despesa', 'Receita'],
+      datasets: [{
+          label: 'Balanço consolidado de depesas e receitas R$',
+          data: [2000, 1000],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.9)',
+              'rgba(75, 192, 192, 0.9)',
+              
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 0.9)',
+              'rgba(75, 192, 192, 0.9)',
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
   }
 });
 
