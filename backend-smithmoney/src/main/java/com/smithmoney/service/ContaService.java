13b2deda4458df649ef387b28b1911933dab359a
Thatiany Steer <steermkt@gmail.com>
2020-11-25 14:46:47 -0300

    Resolvendo conflito da contaService do backend

--- backend-smithmoney/src/main/java/com/smithmoney/service/ContaService.java
+++ backend-smithmoney/src/main/java/com/smithmoney/service/ContaService.java
@@ -43,15 +43,15 @@ public class ContaService {
 	public Conta update(ContaDTO contaDTO) {
 		Conta contaSalva = this.findById(contaDTO.getId());		
 		if(contaDTO.getNome() != null) contaSalva.setNome(contaDTO.getNome());
-<<<<<<< HEAD
-		if(contaDTO.getTipo_Conta()!= null) contaSalva.setTipoConta(contaDTO.getTipo_Conta());
-		
-		if(contaDTO.getTipo_Conta() == TipoConta.Carteira) {
-			contaSalva.setBanco(null);
-		}else {
-			contaSalva.setBanco(contaDTO.getBanco());
-		}		
-=======
+//<<<<<<< HEAD
+//		if(contaDTO.getTipo_Conta()!= null) contaSalva.setTipoConta(contaDTO.getTipo_Conta());
+//		
+//		if(contaDTO.getTipo_Conta() == TipoConta.Carteira) {
+//			contaSalva.setBanco(null);
+//		}else {
+//			contaSalva.setBanco(contaDTO.getBanco());
+//		}		
+//=======
 				
 		if(contaDTO.getTipo_Conta() != contaSalva.getTipoConta()) {
 			if(contaDTO.getTipo_Conta() == TipoConta.Carteira) {
@@ -62,7 +62,7 @@ public class ContaService {
 		}
 		
 		if(contaDTO.getTipo_Conta()!= null) contaSalva.setTipoConta(contaDTO.getTipo_Conta());
->>>>>>> 2f3702a78cfbbde326aa0d15c6dd16c0c748b2a1
+
 		
 		return this.contaRepository.save(contaSalva);
 	}
