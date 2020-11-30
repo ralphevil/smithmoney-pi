09fb244788f1c7e1095b97c9192556f338de30a9
Raphael Souza <ralphevils5@gmail.com>
2020-11-30 00:40:08 -0300

    Restrições de Inputs

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -1,15 +1,27 @@
+const senhaInput = document.querySelector("#senha");
+const confirmarInput = document.querySelector("#confirm_senha");
+
+confirmarInput.addEventListener("focusout",()=>{
+  if(senhaInput.value != confirmarInput.value){
+    toastr.error("As senhas não são iguais!");
+    senhaInput.value = "";
+    confirmarInput.value = "";
+  }
+})
+
 function cadastrarUsuario(event, form) {
     event.preventDefault();
     const nome = form.nome.value;
     const email = form.email.value;
     const senha = form.senha.value;
+    const confirmar = form.confirm_senha.value;
     const data = {
       nome: nome,
       email: email,
       password: senha
     }
 
-    if ((!nome.length <= 0 || !nome == "") && (!email.length <= 0 || !email == "") && (!senha.length <= 0 || !senha == 0)) {
+    if ((!nome.length <= 0 || !nome == "") && (!email.length <= 0 || !email == "") && (!senha.length <= 0 || !senha == 0) && (!confirmar.length <= 0 || !confirmar == 0)) {
       fetch(url+"/api/usuarios", {
         headers: {
           "Content-Type": "application/json"
